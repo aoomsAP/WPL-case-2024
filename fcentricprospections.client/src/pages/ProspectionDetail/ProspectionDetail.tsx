0a85e6d79f0f8c4536ffeb3e1545cd002171323e
Tatjana Torchenyuk <tatjana.torchenyuk@student.ap.be>
2024-11-09 23:13:59 +0100

    Redefined load func for prospectionDetail, shopDetail, visitType and contactType + useEffect with promise.all

--- fcentricprospections.client/src/pages/ProspectionDetail/ProspectionDetail.tsx
+++ fcentricprospections.client/src/pages/ProspectionDetail/ProspectionDetail.tsx
@@ -1,26 +1,24 @@
-import { useParams, useLocation, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import {
   IContactType,
   IProspectionDetail,
   IVisitType,
-  Prospection,
   Shop,
 } from "../../types";
 import styles from "./ProspectionDetail.module.css";
 
 export const ProspectionDetail = () => {
-  const [loading, setLoading] = useState(true);
-  const [prospectionDetail, setProspectionDetail] =
-    useState<IProspectionDetail>();
+  const [prospectionDetail, setProspectionDetail] = useState<IProspectionDetail>();
   const [shopDetail, setShopDetail] = useState<Shop>();
   const [contactType, setContactType] = useState<IContactType>();
   const [visitType, setVisitType] = useState<IVisitType>();
   const { shopId, prospectionId } = useParams(); // Extract the IDs from the URL for the prospection as the store
-  const navigate = useNavigate();
 
-  // Load prospection detail data
-  async function loadProspection(prospectionId: string) {
+
+
+async function loadProspection(prospectionId: string):Promise<IProspectionDetail> {
+    let result: IProspectionDetail = {} as IProspectionDetail;
     try {
       const response = await fetch(`/api/prospections/${prospectionId}`, {
         method: "GET", // Specify the method if it's not 'GET' by default
@@ -28,17 +26,20 @@ export const ProspectionDetail = () => {
           "Content-Type": "application/json", // Define the expected content type
         },
       });
-      console.log(response);
-      const json: IProspectionDetail = await response.json();
 
-      setProspectionDetail(json);
+     result = await response.json();
+      
     } catch (error) {
-      console.log(error); // to do navigatie naar een error pagina
+      console.log("Error fetching prospection data:", error);
     }
+
+    return result;
   }
 
-  // Load shop detail data
-  async function loadShopDetail(shopId: string) {
+async function loadShopDetail(shopId: string):Promise<Shop> {
+
+    let result = {} as Shop;
+
     try {
       const response = await fetch(`/api/shops/${shopId}`, {
         method: "GET", // Specify the method if it's not 'GET' by default
@@ -47,33 +48,19 @@ export const ProspectionDetail = () => {
         },
       });
 
-      const responseProspection = await fetch(
-        `/api/shops/${shopId}/prospections`,
-        {
-          method: "GET", // Specify the method if it's not 'GET' by default
-          headers: {
-            "Content-Type": "application/json", // Define the expected content type
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to fetch shop data");
-      }
-
       const json: Shop = await response.json();
-      setShopDetail(json);
+      result = json;
 
-      if (!responseProspection.ok) {
-        throw new Error("Failed to ftech prospection data");
-      }
     } catch (error) {
       console.error("Error fetching shop data:", error);
     }
+
+    return result;
   }
 
-  // load contactType object by contactTypeId
-  async function loadContactType(contactTypeId: string) {
+  async function loadContactType(contactTypeId: string):Promise<IContactType> {
+    let result = {} as IContactType;
+
     try {
       const response = await fetch(`/api/contacttypes`, {
         method: "GET", // Specify the method if it's not 'GET' by default
@@ -81,17 +68,25 @@ export const ProspectionDetail = () => {
           "Content-Type": "application/json", // Define the expected content type
         },
       });
-      console.log(response);
-      const json: IContactType[] = await response.json();
 
-      setContactType(json.find((c) => c.id == contactTypeId));
+      const json: IContactType[] = await response.json();
+        let contantType = json.find((c) => c.id == contactTypeId);
+      if (contantType != undefined) {
+        result = contantType;
+      }
+        
+
     } catch (error) {
-      console.log(error); // to do navigatie naar een error pagina
+      console.log(error);
     }
+
+    return result;
   }
 
-  // load visitType object by visitTypeId
-  async function loadVisitType(visitTypeId: string) {
+
+async function loadVisitType(visitTypeId: string):Promise<IVisitType> {
+    let result = {} as IVisitType;
+
     try {
       const response = await fetch(`/api/visittypes`, {
         method: "GET", // Specify the method if it's not 'GET' by default
@@ -99,24 +94,44 @@ export const ProspectionDetail = () => {
           "Content-Type": "application/json", // Define the expected content type
         },
       });
-      console.log(response);
-      const json: IVisitType[] = await response.json();
 
-      setVisitType(json.find((v) => v.id == visitTypeId));
+      const json: IVisitType[] = await response.json();
+        let visitType = json.find((c) => c.id == visitTypeId);
+      if (visitType != undefined) {
+        result = visitType;
+      }
     } catch (error) {
-      console.log(error); // to do navigatie naar een error pagina
+      console.log(error);
     }
+
+    return result;
   }
 
   useEffect(() => {
+   
+    const getAllData = async () => {
+        if(prospectionId && shopId) {
+       
+            let [result1, result2] = await Promise.all([loadProspection(prospectionId), loadShopDetail(shopId)])
 
-  });
+            if (result1) {
+                let [result3, result4] = await Promise.all([loadContactType(result1.contactPersonTypeId), loadVisitType(result1.visitTypeId)])
+                
+                setProspectionDetail(result1);
+                setShopDetail(result2);
+                setContactType(result3);
+                setVisitType(result4);
+            }
+         
+        }
+    }
+     
+    getAllData();
+    
+
+  }, []);
 
 
-
-  // if (!state) {
-  //     return <div>Loading...</div>;
-  // }
   console.log(prospectionDetail);
 
   return (
@@ -136,6 +151,4 @@ export const ProspectionDetail = () => {
   );
 };
 
-// function useState(): [any, any] {
-//     throw new Error("Function not implemented.");
-// }
+
