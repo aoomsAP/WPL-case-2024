54d6b6467b9cd18463e446ce5b466a7e9b62f81f
Tatjana Torchenyuk <tatjana.torchenyuk@student.ap.be>
2024-12-07 21:39:48 +0100

    Queries and endpoints for Contacts, Addresses, Cities and NewShop.

---
+++ FCentricProspections.Server/DomainModels/CityDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FCentricProspections.Server.DomainModels
+{
+    public class CityDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string PostalCode { get; set; }
+        public long CountryId { get; set; }
+        public CountryDto Country {  get; set; }
+    }
+}
---
+++ FCentricProspections.Server/ViewModels/AddressViewModels.cs
@@ -0,0 +1,31 @@
+﻿using FCentricProspections.Server.DataModels;
+
+namespace FCentricProspections.Server.ViewModels
+{
+    public class AddressCreateViewModel
+    {
+        public string Street1 { get; set; }
+
+        public long? CityId { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public long UserCreatedId { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+    }
+    public class AddressGetViewModel
+    {
+        public long Id { get; set; }
+
+        public string Street1 { get; set; }
+
+        public long? CityId { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+    }
+}
