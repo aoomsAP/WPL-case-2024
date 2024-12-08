182e4285bd42f74bcd15aaa0ebba24bb980b5a52
Alki Ooms <alki.ooms@student.ap.be>
2024-12-08 05:11:49 +0100

    Additional responsiveness

--- fcentricprospections.client/src/pages/CalendarPage/CalendarPage.tsx
+++ fcentricprospections.client/src/pages/CalendarPage/CalendarPage.tsx
@@ -6,55 +6,65 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import { EmployeeSelect } from './EmployeeSelect';
 import { EventInput } from '@fullcalendar/core';
 import { Oval } from 'react-loader-spinner';
-import style from './CalenderPage.module.css';
+import styles from './CalenderPage.module.css';
 import listPlugin from '@fullcalendar/list';
+import nlLocale from '@fullcalendar/core/locales/nl';
 
 export const CalendarPage = () => {
     const { appointments } = useContext(UserContext);
-    const [events, setEvents] = useState<EventInput[]>([]);
-    const calendarRef = useRef<FullCalendar | null>(null);
-    const dimensions = getWindowDimensions();
 
-    function getWindowDimensions() {
-        const { innerWidth: width, innerHeight: height } = window;
-        return {
-          width,
-          height
-        };
-      }
+    const [events, setEvents] = useState<EventInput[]>([]);
+    const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+
+    const calendarRef = useRef<FullCalendar | null>(null);
+
+    function isAllDay(startDate: Date, endDate: Date): boolean {
+        const businessStartHour = 9; // 9 AM
+        const businessEndHour = 18; // 6 PM
     
+        return (
+            startDate.getHours() <= businessStartHour &&
+            endDate.getHours() >= businessEndHour &&
+            startDate < endDate
+        );
+    }
+
     useEffect(() => {
         const userEvents = appointments.map((appointment) => ({
             title: appointment.name ? appointment.name : (appointment.remarks ? appointment.remarks : "Geen details"),
             start: appointment.startDate,
             end: appointment.endDate,
             color: "steelblue",
+            allDay: isAllDay(new Date(appointment.startDate), new Date(appointment.endDate))
         }));
         setEvents(userEvents);
     }, [appointments]);
 
     const handleWindowResize = (arg: { view: { type: string } }) => {
-        console.log(`The calendar adjusted to a window resize. Current view: ${arg.view.type}`);
+        setWindowWidth(window.innerWidth);
     };
 
     return (
-        <div className={style.calendarContainer}>
+        <div className={styles.container}>
             {events.length > 1 ? (
                 <>
-                    <div className={style.topControls}>
+                    <div className={styles.employeeSelect}>
                         <EmployeeSelect setEvents={setEvents} />
                     </div>
+
                     <FullCalendar
                         ref={calendarRef}
-                        plugins={[dayGridPlugin, timeGridPlugin , listPlugin ]}
-                        initialView={ dimensions.width > 700 ? "dayGridMonth" : "listMonth"}
+                        plugins={[dayGridPlugin, timeGridPlugin, listPlugin]}
+                        initialView={windowWidth > 700 ? "dayGridMonth" : "listMonth"}
                         weekends={false}
                         navLinks={true}
+                        locale={nlLocale} // NL localization (date & time formatting)
                         headerToolbar={{
                             left: "prev,next today",
                             center: "title",
-                            right:`${dimensions.height > 700 ? 'dayGridMonth,timeGridWeek,timeGridDay' : 'dayGridMonth,listMonth,timeGridDay'}`
+                            right: `${windowWidth > 700 ? 'dayGridMonth,timeGridWeek,timeGridDay' : 'listMonth,timeGridDay'}`
                         }}
+                        slotMinTime="07:00:00" // Start timegrid from 7am
                         slotDuration="00:30:00"
                         progressiveEventRendering
                         events={events}
